d099df502659e124c1f77deacd7449957730c415
DoHyeong, Jung <46253412+DHelloper@users.noreply.github.com>
2023-06-06 20:54:34 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 384 ms, Memory: 18984 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] map1 = new int[N][M];
+        int[][] map2 = new int[N][M];
+
+        for(int i=0; i<N; i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++)
+            {
+                map1[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i=0; i<N; i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++)
+            {
+                map2[i][j] = Integer.parseInt(st.nextToken())+map1[i][j];
+                System.out.print(map2[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
