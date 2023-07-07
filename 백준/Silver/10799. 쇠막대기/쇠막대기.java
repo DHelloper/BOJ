2d6993a1a483e7bf561232d170e13d8f67e6631b
DoHyeong, Jung <46253412+DHelloper@users.noreply.github.com>
2023-07-07 15:58:37 +0900

    [Silver II] Title: 쇠막대기, Time: 188 ms, Memory: 16276 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        Stack<Character> stack = new Stack<>();
+
+        int result = 0;
+        for(int i=0; i<str.length(); i++)
+        {
+            if(str.charAt(i)=='(')
+            {
+                stack.push('(');
+                continue;
+            }
+            if(str.charAt(i)==')')
+            {
+                stack.pop();
+                if(str.charAt(i-1)=='(')
+                {
+                    result += stack.size();
+                }
+                else
+                {
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+
+    }
+}
