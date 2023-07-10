86daa64e5e4337d7b279d2cb91cb43a308682274
DoHyeong, Jung <46253412+DHelloper@users.noreply.github.com>
2023-07-10 16:55:53 +0900

    [Silver I] Title: 절댓값 힙, Time: 580 ms, Memory: 26688 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) -> {
+            if(Math.abs(o1)==Math.abs(o2))
+            {
+                return o1-o2;
+            }
+            return Math.abs(o1)-Math.abs(o2);
+        });
+        for(int i=0; i<N; i++)
+        {
+            int num = Integer.parseInt(br.readLine());
+            if(num==0)
+            {
+                if(pq.isEmpty())
+                {
+                    System.out.println("0");
+                }
+                else
+                {
+                    System.out.println(pq.poll());
+                }
+                continue;
+            }
+            pq.add(num);
+        }
+
+    }
+}
