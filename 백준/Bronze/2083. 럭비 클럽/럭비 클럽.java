5b870f31ef0fcb62b7f222681e6b7e5d91c7174f
DoHyeong, Jung <46253412+DHelloper@users.noreply.github.com>
2023-07-24 22:11:49 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 128 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            if(str.equals("#") && age==0 && weight==0)
+                break;
+            if(age>17 || weight >= 80)
+                System.out.println(str + " Senior");
+            else
+                System.out.println(str + " Junior");
+        }
+
+
+    }
+}
