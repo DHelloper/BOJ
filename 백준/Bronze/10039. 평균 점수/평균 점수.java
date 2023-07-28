fd989ab86778095dcb7b79bffd0bc711d2f98205
DoHyeong, Jung <46253412+DHelloper@users.noreply.github.com>
2023-07-28 15:42:55 +0900

    [Bronze IV] Title: 평균 점수, Time: 120 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[5];
+        int result=0;
+        for(int i=0; i<5; i++)
+        {
+           arr[i] = Integer.parseInt(br.readLine());
+           if(arr[i] < 40)
+           {
+               arr[i] = 40;
+           }
+           result+=arr[i];
+        }
+        System.out.println(result/5);
+
+    }
+}
